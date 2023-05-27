201a3bcd3b52beeeccd8626953938c3aee4bcf49
Steve Romero <romero.steve.a@gmail.com>
2023-05-26 18:00:04 -0700

    complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const navlist = links.map((link) => {
+    const l = "#"+link;
+    return <a key={link} href={l}>{link}</a>;
+  });
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{navlist}</nav>;
 }
 
 export default NavBar;
